1ff683a756cc09fe1792965b18360f099a6123cf
Zhannur <jannuraidynuly@gmail.com>
2022-02-04 06:42:48 +0500

    redux thunk and async actions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
+import { fetchCustomers } from './asyncActions/customers';
 
 function App() {
   const dispatch = useDispatch();
@@ -34,7 +35,7 @@ function App() {
         <button onClick={() => addMoney(Number(prompt()))}>Пополнить счет</button>
         <button onClick={() => getMoney(Number(prompt()))}>Снять с счета</button>
         <button onClick={() => addCustomer(prompt())}>Добавить клиента</button>
-        <button onClick={() => getMoney(Number(prompt()))}>Удалить клиента</button>
+        <button onClick={() => dispatch(fetchCustomers())}>Получить клиентов из базы</button>
       </div>
       {customers.length > 0
           ?
--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -1,5 +1,6 @@
 const ADD_CUSTOMER = "ADD_CUSTOMER";
 const REMOVE_CUSTOMER = "REMOVE_CUSTOMER";
+const ADD_MANY_CUSTOMERS = "ADD_MANY_CUSTOMERS";
 
 const defaultState = {
     customers: []
@@ -7,6 +8,8 @@ const defaultState = {
 
 export const customerReducer = (state = defaultState, action) => {
     switch(action.type) {
+        case ADD_MANY_CUSTOMERS:
+            return {...state, customers: [...state.customers, ...action.payload]}
         case ADD_CUSTOMER:
             return {...state, customers: [...state.customers, action.payload]}
         case REMOVE_CUSTOMER:
@@ -17,4 +20,5 @@ export const customerReducer = (state = defaultState, action) => {
 }
 
 export const addCustomerAction = (payload) => ({type: ADD_CUSTOMER, payload})
+export const addManyCustomersAction = (payload) => ({type: ADD_MANY_CUSTOMERS, payload})
 export const removeCustomerAction = (payload) => ({type: REMOVE_CUSTOMER, payload})
