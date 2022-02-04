0db6f988473f388b3a211460f5b7e668af8fbade
Zhannur <jannuraidynuly@gmail.com>
2022-02-04 06:15:04 +0500

    Redux.Action creater, Job with massive and Refactoring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 
 function App() {
   const dispatch = useDispatch();
   const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
 
   const addMoney = (cash) => {
       dispatch({type: "ADD_CASH", payload: cash})
@@ -13,13 +15,39 @@ function App() {
       dispatch({type: "GET_CASH", payload: cash})
   }
 
+  const addCustomer = (name) => {
+    const customer = {
+      name: name,
+      id: Date.now()
+    }
+    dispatch(addCustomerAction(customer))
+  }
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
+  }
+
   return (
     <div className="App">
       <div style={{fontSize: '2rem'}}>{cash}</div>
       <div style={{display: 'flex'}}>
         <button onClick={() => addMoney(Number(prompt()))}>Пополнить счет</button>
         <button onClick={() => getMoney(Number(prompt()))}>Снять с счета</button>
+        <button onClick={() => addCustomer(prompt())}>Добавить клиента</button>
+        <button onClick={() => getMoney(Number(prompt()))}>Удалить клиента</button>
       </div>
+      {customers.length > 0
+          ?
+          <div>
+            {customers.map(customer => 
+                <div onClick={() => removeCustomer(customer)} style={{fontSize: "2rem", border: "1px solid black", padding: "10 20"}}>
+                  {customer.name}
+                </div>  
+            )}
+          </div>
+          :
+          <div style={{fontSize: '2rem', marginTop: 20}}>Клиенты отсутствует!</div>
+      }
     </div>
   );
 }
